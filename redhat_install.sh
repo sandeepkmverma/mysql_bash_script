a0a09987a37b3bead0c61202b3df117e318ea6a8
root <root@ip-172-31-88-145.ec2.internal>
2019-07-26 07:22:47 +0000

    redhat_installation

--- redhat_install.sh
+++ redhat_install.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-export http_proxy= $Write proxy
-export https_proxy= $ write proxy
-sudo -E yum -y update
-sudo -E yum -y install wget
+yum -y update
+yum -y install wget
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
-sudo  rpm -ivh mysql-community-release-el7-5.noarch.rpm
-sudo -E yum -y update
-sudo -E yum -y install mysql-server
-sudo systemctl start mysqld
+rpm -ivh mysql-community-release-el7-5.noarch.rpm
+yum -y update
+yum -y install mysql-server
+systemctl start mysqld
 sleep 1s
 mysql -u root <<-EOF
 UPDATE mysql.user SET Password=PASSWORD('sandeep') WHERE User='root';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
 FLUSH PRIVILEGES;
 EOF
